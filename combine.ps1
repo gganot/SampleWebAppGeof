5e4ece4a91bf7d63db87662a090a1264c8076b74
Geofrey D. Ganot <gganot@gmail.com>
2023-06-09 21:27:29 +0800

    Add files via upload

---
+++ combine.ps1
@@ -0,0 +1,27 @@
+param(
+    #[string]$path = $(throw "You must enter the path to the .sql files you want to append."),
+    [string]$NewFileName = "Out.sql"
+)
+ 
+$path = "C:\Geofrey\Powershell\Sql"
+$outFile = "$path\$NewFileName"
+ 
+cls
+if((Test-Path $outFile) -eq $true) {Remove-Item -Path $outFile -Force}
+ 
+$files = Get-ChildItem -LiteralPath $path -Include "*.sql" -Recurse | Sort-Object -Property Name
+ 
+New-Item -ItemType file -Path $outFile -Force | Out-Null
+ 
+foreach($file in $files)
+{
+    Write-Host "Appending file $file..." -ForegroundColor Gray
+    $content = Get-Content -Path $file.FullName
+    Add-Content -Path $outFile "----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    Add-Content -Path $outFile "--      $File"
+    Add-Content -Path $outFile "----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    Add-Content -Path $outFile $content
+    Add-Content -Path $outFile "GO`n"
+}
+ 
+Write-Host "Completed file $outFile" -ForegroundColor DarkGreen
