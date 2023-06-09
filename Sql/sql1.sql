dad3561f7c4cbb4599908ff14ccdef29f40d939a
Geofrey D. Ganot <gganot@gmail.com>
2023-06-09 16:38:40 +0800

    Update sql1.sql

--- Sql/sql1.sql
+++ Sql/sql1.sql
@@ -1,3 +1,10 @@
+IF OBJECT_ID('GetEmployeeById_Dacpac_june062023', 'P') IS NOT NULL
+BEGIN
+    DROP PROCEDURE GetEmployeeById_Dacpac_june062023;
+END
+
+GO
+
 CREATE Procedure [dbo].[GetEmployeeById_Dacpac_june062023]
 (@EmployeeID as int)
 
