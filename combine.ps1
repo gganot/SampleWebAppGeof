f2616eeccc6b531fe3a6bf0a4f9804dd208abd6f
Geofrey D. Ganot <gganot@gmail.com>
2023-06-09 21:51:04 +0800

    Update combine.ps1

--- combine.ps1
+++ combine.ps1
@@ -1,1 +1,27 @@
-Write-Host $GITHUB_WORKSPACE
+param(
+    #[string]$path = $(throw "You must enter the path to the .sql files you want to append."),
+    [string]$NewFileName = "Out.sql"
+)
+ 
+$path = "/Sql"
+$outFile = "$path\$NewFileName"
+ 
+cls
+if((Test-Path $outFile) -eq $true) {Remove-Item -Path $outFile -Force}
+ 
+$files = Get-ChildItem -LiteralPath $path -Include "*.sql" -Recurse | Sort-Object -Property Name
+ 
+New-Item -ItemType file -Path $outFile -Force | Out-Null
+ 
+foreach($file in $files)
+{
+    Write-Host "Appending file $file..." -ForegroundColor Gray
+    $content = Get-Content -Path $file.FullName
+    Add-Content -Path $outFile "----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    Add-Content -Path $outFile "--      $File"
+    Add-Content -Path $outFile "----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    Add-Content -Path $outFile $content
+    Add-Content -Path $outFile "GO`n"
+}
+ 
+Write-Host "Completed file $outFile" -ForegroundColor DarkGreen
