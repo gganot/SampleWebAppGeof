5d14395454a0630645bafe2b0d6e2bf1d193394a
Geofrey78 <geofrey@kineticstaff.com>
2023-06-12 20:35:05 +0800

    Push my updates in sql

--- Sql/Run_Sql.sql
+++ Sql/Run_Sql.sql
@@ -2,3 +2,16 @@ Alter table Employee
 Add DailyRate decimal(18,2) null
 Go
 
+
+Alter table Employee 
+Add HourlyRate decimal(17,2) null
+Go
+
+Create Procedure sp_TestJune122023
+
+as
+
+
+select * from [dbo].[Employee]
+
+go
