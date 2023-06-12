85f889f0a21cf19dc0dabf973e13b6556194c5aa
Geofrey78 <geofrey@kineticstaff.com>
2023-06-12 21:07:37 +0800

    Add middle name

--- Sql/Run_Sql.sql
+++ Sql/Run_Sql.sql
@@ -7,6 +7,7 @@ Alter table Employee
 Add HourlyRate decimal(17,2) null
 Go
 
+
 Create Procedure sp_TestJune122023
 
 as
@@ -15,3 +16,7 @@ as
 select * from [dbo].[Employee]
 
 go
+
+Alter table Employee 
+Add MiddleName varchar(100) null
+Go
